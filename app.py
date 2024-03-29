1ac884a8cb6443f61f32bc7dbdd1eb4805672eae
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2024-03-29 17:27:12 +0530

    mini ATS checker

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+
+from dotenv import load_dotenv
+
+load_dotenv() # load all the environment variables
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# gemini pro response
+def get_gemini_pro_response(input_text):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(input_text)
+    return response.text
+
+# taking the pdf and extract the text
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    for page in reader.pages:
+        text += str(page.extract_text())
+    return text
+
+input_prompt="""
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and AI engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving the resumes. Assign the percentage Matching based 
+on Job description and
+the missing keywords with high accuracy
+resume:{text}
+description:{job_description}
+
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+"""
+
+## streamlit app
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+job_description=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
+
+submit = st.button("Submit")
+
+
+if submit:
+    if uploaded_file is not None:
+        text=input_pdf_text(uploaded_file)
+        response=get_gemini_pro_response(input_prompt)
+        st.subheader(response)
